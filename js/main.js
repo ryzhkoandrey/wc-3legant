b6c3ea4ed6eb27085e66d1b4af260c73f61ea781
Андрей Рыжко <ryzhkoandrey@gmail.com>
2024-11-17 12:20:41 +0200

    +

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@ const closeTaglineBtn = document.querySelector('.tagline__close');
 closeTaglineBtn.onclick = function () {
     tagline.remove();
 };
+
+// ---------- ARRIVALS: SWIPER ----------
+
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+    freeMode: true,
+    pagination: {
+        el: '.dots',
+        clickable: true,
+    },
+});
