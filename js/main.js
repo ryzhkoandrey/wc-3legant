b40f16112c800663ec5630f8f7f4978d2b1b4329
Андрей Рыжко <ryzhkoandrey@gmail.com>
2024-11-19 14:59:57 +0200

    +

--- js/main.js
+++ js/main.js
@@ -20,3 +20,22 @@ const swiper = new Swiper('.swiper', {
 });
 
 // ---------- PROMOTION > TIMER ----------
+
+const endDate = new Date('Nov 22, 2024, 12:00:00');
+
+const daysElement = document.querySelector('#days');
+const hoursElement = document.querySelector('#hours');
+const minutesElement = document.querySelector('#minutes');
+const secondsElement = document.querySelector('#seconds');
+
+function updateTimer() {
+    const now = new Date();
+    const timeDifference = endDate - now;
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+};
+
+updateTimer();
